869767e0b194739bfdea68fbcc2f3ac2e1958f9c
Joceima <silverbracesora@gmail.com>
2025-10-30 16:50:45 +0100

    VisualCue qui marche

--- Assets/Scripts/InkIntegration/InkTriggers.cs
+++ Assets/Scripts/InkIntegration/InkTriggers.cs
@@ -1,7 +1,19 @@
+using NUnit.Framework.Internal;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class InkTriggers : MonoBehaviour
 {
+
+    [Header("Visual Cue")]
+    [SerializeField] TextMeshPro TextMeshVisualCue;
+    private bool playerInRange = false;
+
+    [Header("Ink JSON")]
+    [SerializeField] private TextAsset inkJSON;
+  
     public static void ProcessTag(string tag)
     {
         switch(tag)
@@ -21,6 +33,50 @@ public class InkTriggers : MonoBehaviour
             default:
                 Debug.Log("Tag inconnu: " + tag);
                 break;
+        }
+    }
+
+    private void Awake()
+    {
+        Debug.Log("Awake InkTriggers");
+        playerInRange = false;
+        TextMeshVisualCue.gameObject.SetActive(false);
+    }
+
+    private void Update()
+    {
+        Debug.Log("Update InkTriggers playerInranger" + playerInRange);
+        if (playerInRange)
+        {
+            Debug.Log("Player in range - showing visual cue");
+            TextMeshVisualCue.gameObject.SetActive(true);
+            if(Keyboard.current.eKey.wasPressedThisFrame)
+            {
+                Debug.Log("E key pressed - Starting Ink story");
+                Debug.Log(inkJSON.text);
+            }
+        }
+        else
+        {
+            //Debug.Log("Player out of range - hiding visual cue");
+            TextMeshVisualCue.gameObject.SetActive(false);
+        }
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        Debug.Log("Trigger entered by: " + collider.gameObject.name);
+        if(collider.CompareTag("Player"))
+        {
+            playerInRange = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider collider)
+    {
+        if(collider.CompareTag("Player"))
+        {
+            playerInRange = false;
         }
     }
 }
